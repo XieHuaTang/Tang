a8de5dcf435d9f4ad2d325bfa72820c29de7e0a5
Mr.Tang <2025147846@qq.com>
2019-09-16 20:58:40 +0800

    第二次随便提交

--- app/src/main/java/com/example/asus/myapplication/MainActivity.java
+++ app/src/main/java/com/example/asus/myapplication/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    //我是修改的那部分
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
